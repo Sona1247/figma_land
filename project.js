a83b19a84e996fb84ddceea1277f7298777559d4
Sona <sona30e@gmail.com>
2021-04-17 01:50:09 -0700

    finished eighth part

---
+++ project.js
@@ -0,0 +1,21 @@
+      
+      function initMap() {
+        const azerbaijan = { lat:   40.39031076086174 , lng: 49.83018757137912};
+        const map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 16,
+          center: azerbaijan,
+          disableDefaultUI: true
+        
+        });
+        const marker = new google.maps.Marker({
+          position: azerbaijan,
+          map: map,
+        });
+        navigator.geolocation.getCurrentPosition(onLocationSuccess);
+      }
+      
+  function onLocationSuccess(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    console.log(latitude, longitude);
+  }
