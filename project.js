5c7aed58c837e70b6900ecbc3224d3bdaba59697
Sona <sona30e@gmail.com>
2021-04-21 03:03:33 -0700

    finished animations

--- project.js
+++ project.js
@@ -25,3 +25,43 @@
   hamburger.addEventListener("click", () =>{
     navLinks.classList.toggle("show")
   })
+
+
+  const headerImg = document.querySelector(".imageContent");
+  const headerText = document.querySelector(".textContent");
+  const headerSide = document.querySelector(".header-side");
+  const adviceSide = document.querySelector(".advice-side");
+  const computerSideTitle = document.querySelector(".computer-side-title");
+  const computerSideSubtitle = document.querySelector(".computer-side-subtitle");
+const thirdPartButton = document.querySelector(".thirdPart-button")
+const contentSide = document.querySelector(".content-side")
+const actionSide = document.querySelector(".action-side")
+const partnersSide = document.querySelector(".partners")
+const clientSide = document.querySelector(".client")
+const fifthPartButton = document.querySelector(".fifth-part button")
+const options  = {
+  rootMargin: '20px'
+}
+  function handleIntersection(entries) {
+    entries.map((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+     
+    });
+  };
+
+  const observer = new IntersectionObserver(handleIntersection, options);
+
+  observer.observe(headerImg);
+  observer.observe(headerText);
+  observer.observe(headerSide);
+  observer.observe(adviceSide);
+  observer.observe(computerSideTitle);
+  observer.observe(computerSideSubtitle);
+  observer.observe(thirdPartButton);
+  observer.observe(contentSide);
+  observer.observe(actionSide);
+  observer.observe(clientSide);
+  observer.observe(partnersSide);
+  observer.observe(fifthPartButton)
