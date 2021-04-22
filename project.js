fb4fa78bef9353e92b437f1d62bfb84db5d8a246
Sona <sona30e@gmail.com>
2021-04-22 09:46:54 -0700

    changed the styles of  buttons and links

--- project.js
+++ project.js
@@ -12,13 +12,20 @@ function initMap() {
   navigator.geolocation.getCurrentPosition(onLocationSuccess);
 }
 
-
+function onLocationSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  console.log(latitude, longitude);
+}
 const hamburger = document.getElementById("hamburger-menu-button");
 const navLinks = document.querySelector(".navbar-second-side");
-
+const navbarClose= document.querySelector(".navbar-close");
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("show");
 });
+navbarClose.addEventListener("click", () => {
+  navLinks.classList.remove("show")
+ });
 
 const headerImg = document.querySelector(".imageContent");
 const headerText = document.querySelector(".textContent");
@@ -85,3 +92,39 @@ observer.observe(icon);
 observer.observe(footerIcon);
 observer.observe(pagesLinks);
 observer.observe(map);
+
+
+
+const infoLink = document.querySelector(".info-link");
+const contactLink = document.querySelector(".contact-link");
+const priceLink = document.querySelector(".price-link");
+const productLink = document.querySelector(".product-link");
+
+
+const seventhPart = document.querySelector(".seventh-part");
+const lastPart = document.querySelector(".last-part");
+const eighthPart = document.querySelector(".eighth-part");
+const fifthPart = document.querySelector(".fifth-part");
+
+
+  priceLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    seventhPart.scrollIntoView({behavior:'smooth', block:'start'})
+    navLinks.classList.remove('show');
+  });
+
+  infoLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    lastPart.scrollIntoView({behavior:'smooth', block:'start'})
+    navLinks.classList.remove('show');
+  });
+  contactLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    eighthPart.scrollIntoView({behavior:'smooth', block:'start'})
+    navLinks.classList.remove('show');
+  });
+  productLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    fifthPart.scrollIntoView({behavior:'smooth', block:'start'})
+    navLinks.classList.remove('show');
+  });
